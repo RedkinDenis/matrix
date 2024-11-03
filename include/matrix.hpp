613569a9c0a7f4c28e58c7dd80c422a0b17a94f0
RedkinDenis <zapobedu1075@gmail.com>
2024-11-03 12:52:38 +0300

    refactoring

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -116,60 +116,27 @@ private:
         return true;
     }  
 
+    template <typename StreamType>
+    MyVector<MyVector<T>> getMatrixData(StreamType& inp) {
+        T tmp = 0;
+        size_t sz = 0;
+        inp >> sz;
+
+        MyVector<MyVector<T>> data;
+
+        for (size_t i = 0; i < sz; i++) {
+            MyVector<T> vec;
+            for (size_t j = 0; j < sz; j++) {
+                inp >> tmp;
+                vec.push_back(tmp);
+            }
+            data.push_back(vec);
+        }
+
+        return data;
+    }
+
     MyVector<MyVector<T>> getMatrixData() {
-        T tmp = 0;
-        size_t sz = 0;
-        std::cin >> sz;
-
-        MyVector<MyVector<T>> data;
-
-        for (size_t i = 0; i < sz; i++) {
-            MyVector<T> vec;
-            for (size_t j = 0; j < sz; j++) {
-                std::cin >> tmp;
-                vec.push_back(tmp);
-            }
-            data.push_back(vec); 
-        }
-
-        return data;
-    }
-
-    MyVector<MyVector<T>> getMatrixData (std::ifstream &inp) {
-        T tmp = 0;
-        size_t sz = 0;
-        inp >> sz;
-
-        MyVector<MyVector<T>> data;
-
-        for (size_t i = 0; i < sz; i++) {
-            MyVector<T> vec;
-            for (size_t j = 0; j < sz; j++) {
-                inp >> tmp;
-                vec.push_back(tmp);
-            }
-            data.push_back(vec); 
-        }
-
-        return data;
-    }
-
-    MyVector<MyVector<T>> getMatrixData (std::stringstream &inp) {
-        T tmp = 0;
-        size_t sz = 0;
-        inp >> sz;
-
-        MyVector<MyVector<T>> data;
-
-        for (size_t i = 0; i < sz; i++) {
-            MyVector<T> vec;
-            for (size_t j = 0; j < sz; j++) {
-                inp >> tmp;
-                vec.push_back(tmp);
-            }
-            data.push_back(vec); 
-        }
-
-        return data;
+        return getMatrixData(std::cin);
     }
 };
